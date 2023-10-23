7fedc4775bc17839d88abeb62af823c8f792de2f
Roger <Roger.Stich@gmx.de>
2023-10-23 20:47:10 +0200

    extracted logic for better readability

--- Tiny.Cli/Validation/CombinationOfArgumentValidator.cs
+++ Tiny.Cli/Validation/CombinationOfArgumentValidator.cs
@@ -9,11 +9,7 @@ public class CombinationOfArgumentValidator : BaseValidator, IArgumentValidator
     public WorkFlowParameters ValidateArguments(string[] arguments, WorkFlowParameters parameters)
     {
         var singleFile = ContainsValidatorArgument(arguments, Parameter.SingleFile.Complex, Parameter.SingleFile.Simple);
-        var directory = ContainsValidatorArgument(arguments, Parameter.Directory.Complex, Parameter.Directory.Simple);
-        var currentDir = ContainsValidatorArgument(arguments, Parameter.CurrentDirectory.Complex, Parameter.CurrentDirectory.Simple);
-        var subDir = ContainsValidatorArgument(arguments, Parameter.SubDirectory.Complex, Parameter.SubDirectory.Simple);
-        
-        var directoryProvided = directory || currentDir || subDir;
+        var directoryProvided = DirectoryProvided(arguments);
 
         if (!singleFile || !directoryProvided)
         {
@@ -24,5 +20,16 @@ public class CombinationOfArgumentValidator : BaseValidator, IArgumentValidator
         IsValid = false;
         Message = "There cant be both parameters for single file and directory";
         return parameters;
+    }
+
+    private bool DirectoryProvided(string[] arguments)
+    {
+        var directory = ContainsValidatorArgument(arguments, Parameter.Directory.Complex, Parameter.Directory.Simple);
+        var currentDir =
+            ContainsValidatorArgument(arguments, Parameter.CurrentDirectory.Complex, Parameter.CurrentDirectory.Simple);
+        var subDir = ContainsValidatorArgument(arguments, Parameter.SubDirectory.Complex, Parameter.SubDirectory.Simple);
+
+        var directoryProvided = directory || currentDir || subDir;
+        return directoryProvided;
     }
 }
