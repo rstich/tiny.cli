702827145abb749f934b50fa44fb4c02ccf2b3f6
Roger Stich <r.stich@liebensteiner.de>
2023-10-24 14:19:17 +0200

    Added Tests for ArgumentValidatonService

---
+++ tiny.tests/ServiceTests/ArgumentValidatorTests.cs
@@ -0,0 +1,50 @@
+using NSubstitute;
+using Tiny.Cli.Misc;
+using Tiny.Cli.Service;
+using Tiny.Cli.Validation;
+
+namespace tiny.tests.ServiceTests;
+
+public class ArgumentValidatorTests
+{
+    private IEnumerable<IArgumentValidator> CreateCollectionOfValidators(int numberOfValidators, bool invalidValidators = false, int factor = 2)
+    {
+        var retVal = new List<IArgumentValidator>();
+        
+        for (int i = 0; i < numberOfValidators; i++)
+        {
+            var current = Substitute.For<IArgumentValidator>();
+            current.IsValid.Returns(true);
+            if (invalidValidators && i % factor == 0)
+            {
+                current.Message.Returns("Failure");
+                current.IsValid.Returns(false);
+            }
+            
+            retVal.Add(current);
+        }
+
+        return retVal;
+    }
+    
+    [Fact]
+    public void InValidValidator_ThrowsException()
+    {
+        var testData = CreateCollectionOfValidators(10, true, 2);
+        var sut = new ArgumentValidationService(testData);
+        var arguments = Array.Empty<string>();
+        
+        Assert.Throws<InvalidParametersException>(() => sut.ValidateArgumentsAndParseParameters(arguments));
+    }
+    
+    [Fact]
+    public void ValidValidator_ReturnsWorkFlowParameters()
+    {
+        var testData = CreateCollectionOfValidators(10);
+        var sut = new ArgumentValidationService(testData);
+        var arguments = Array.Empty<string>();
+        var response = sut.ValidateArgumentsAndParseParameters(arguments);
+
+        Assert.NotNull(response);
+    }
+}
